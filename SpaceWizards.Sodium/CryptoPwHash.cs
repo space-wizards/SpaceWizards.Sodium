bd20346d3aa0ce99396772e5c2c45546fd58d66c
juliangiebel <juliangiebel@live.de>
2025-10-31 12:35:39 +0100

    Implement wrapper for pwhash derive

---
+++ SpaceWizards.Sodium/CryptoPwHash.cs
@@ -0,0 +1,79 @@
+﻿namespace SpaceWizards.Sodium;
+
+using static Interop.Libsodium;
+
+public static class CryptoPwHash
+{
+    static CryptoPwHash()
+    {
+        SodiumCore.EnsureInit();
+    }
+
+    public const int SaltBytes = (int)crypto_pwhash_SALTBYTES;
+    public const int PasswdMin = (int)crypto_pwhash_PASSWD_MIN;
+    public const int BytesMin = (int)crypto_pwhash_BYTES_MIN;
+
+    public const int AlgDefault = crypto_pwhash_ALG_DEFAULT;
+    public const int AlgArgon2I13 = crypto_pwhash_ALG_ARGON2I13;
+    public const int AlgArgon2Id13 = crypto_pwhash_ALG_ARGON2ID13;
+
+    public const uint MemLimitMin = crypto_pwhash_MEMLIMIT_MIN;
+    public const ulong MemLimitMax = crypto_pwhash_MEMLIMIT_MAX;
+    public const long MemLimitInteractive = crypto_pwhash_MEMLIMIT_INTERACTIVE;
+    public const long MemLimitModerate = crypto_pwhash_MEMLIMIT_MODERATE;
+    public const long MemLimitSensitive = crypto_pwhash_MEMLIMIT_SENSITIVE;
+
+    public const uint OpsLimitMin = crypto_pwhash_OPSLIMIT_MIN;
+    public const ulong OpsLimitMax = crypto_pwhash_OPSLIMIT_MAX;
+    public const long OpsLimitInteractive = crypto_pwhash_OPSLIMIT_INTERACTIVE;
+    public const long OpsLimitModerate = crypto_pwhash_MEMLIMIT_MODERATE;
+    public const long OpsLimitSensitive = crypto_pwhash_MEMLIMIT_SENSITIVE;
+
+
+
+    public static unsafe bool Derive(
+        Span<byte> key,
+        ReadOnlySpan<byte> salt,
+        ReadOnlySpan<sbyte> password,
+        ulong opsLimit,
+        ulong memLimit,
+        int alg)
+    {
+        // Libsodiums pwhash max output length is greater than Int32.MaxValue
+        if (key.Length < BytesMin)
+            throw new ArgumentException($"Output must be at least {nameof(BytesMin)} bytes");
+
+        if (salt.Length != SaltBytes)
+            throw new ArgumentException($"Salt must be {nameof(SaltBytes)} bytes");
+
+        // Libsodiums pwhash max password length is greater than Int32.MaxValue
+        if (password.Length < PasswdMin)
+            throw new ArgumentException($"Output must be at least {nameof(PasswdMin)} bytes");
+
+        if (opsLimit is < OpsLimitMin or > OpsLimitMax)
+            throw new ArgumentException("OpsLimit is invalid size");
+
+        if (memLimit is < MemLimitMin or > MemLimitMax)
+            throw new ArgumentException("MemLimit is invalid size");
+
+        if (alg is not AlgDefault and not AlgArgon2I13 and not AlgArgon2Id13 )
+            throw new ArgumentException("Algorithm is invalid");
+
+        fixed (byte* k = key)
+        fixed (byte* s = salt)
+        fixed (sbyte* p = password)
+        {
+            var ret = crypto_pwhash(
+                k,
+                (ulong)key.Length,
+                p,
+                (ulong)password.Length,
+                s,
+                opsLimit,
+                (nuint)memLimit,
+                alg);
+
+            return ret == 0;
+        }
+    }
+}
