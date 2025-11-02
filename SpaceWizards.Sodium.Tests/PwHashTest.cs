c56849f26f263385464e46159d4d909effa4fdaa
Julian Giebel <juliangiebel@live.de>
2025-11-03 00:35:07 +0100

    Implement wrapper for the pwhash key derivation function (#2)
    * Implement wrapper for pwhash derive
    
    * Implement test for pwhash wrapper
    
    * Update libsodium dependency for arm support
    
    * Change PwHash algorithms to be an enum
    
    * Change size to count

---
+++ SpaceWizards.Sodium.Tests/PwHashTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace SpaceWizards.Sodium.Tests;
+
+[TestFixture]
+public class PwHashTest
+{
+    [Test]
+    public unsafe void Test()
+    {
+        const string password = "Correct Horse Battery Staple";
+        const string expectedString = "DA8885E9B042F702B21812FB9A79C73F92C39C5FA1B9B5F95820C3C1B98A3850";
+        var expected = Convert.FromHexString(expectedString);
+
+        const string saltString = "D32A7FAF80A45FFACC900CA2AE0D4BE5";
+        ReadOnlySpan<byte> saltSpan = new Span<byte>(Convert.FromHexString(saltString));
+        Span<byte> key = stackalloc byte[32];
+
+        var success = CryptoPwHash.Derive(key, saltSpan, Encoding.UTF8.GetBytes(password));
+
+        Assert.That(success, Is.True);
+        Assert.That(key.ToArray(), Is.EquivalentTo(expected));
+    }
+}
---
+++ SpaceWizards.Sodium/CryptoPwHash.cs
@@ -0,0 +1,77 @@
+﻿namespace SpaceWizards.Sodium;
+
+using static Interop.Libsodium;
+
+public static class CryptoPwHash
+{
+    static CryptoPwHash()
+    {
+        SodiumCore.EnsureInit();
+    }
+
+    public const int SaltBytes = (int)crypto_pwhash_SALTBYTES;
+    public const int PasswdMin = (int)crypto_pwhash_PASSWD_MIN;
+    public const int BytesMin = (int)crypto_pwhash_BYTES_MIN;
+
+    public const uint MemLimitMin = crypto_pwhash_MEMLIMIT_MIN;
+    public const ulong MemLimitMax = crypto_pwhash_MEMLIMIT_MAX;
+    public const long MemLimitInteractive = crypto_pwhash_MEMLIMIT_INTERACTIVE;
+    public const long MemLimitModerate = crypto_pwhash_MEMLIMIT_MODERATE;
+    public const long MemLimitSensitive = crypto_pwhash_MEMLIMIT_SENSITIVE;
+
+    public const uint OpsLimitMin = crypto_pwhash_OPSLIMIT_MIN;
+    public const ulong OpsLimitMax = crypto_pwhash_OPSLIMIT_MAX;
+    public const long OpsLimitInteractive = crypto_pwhash_OPSLIMIT_INTERACTIVE;
+    public const long OpsLimitModerate = crypto_pwhash_MEMLIMIT_MODERATE;
+    public const long OpsLimitSensitive = crypto_pwhash_MEMLIMIT_SENSITIVE;
+
+    public static unsafe bool Derive(
+        Span<byte> key,
+        ReadOnlySpan<byte> salt,
+        ReadOnlySpan<byte> password,
+        ulong opsLimit = OpsLimitInteractive,
+        ulong memLimit = MemLimitInteractive,
+        PwHashAlgorithm alg = PwHashAlgorithm.AlgDefault)
+    {
+        // Libsodiums pwhash max output length is greater than Int32.MaxValue
+        if (key.Length < BytesMin)
+            throw new ArgumentException($"Output must be at least {nameof(BytesMin)} bytes");
+
+        if (salt.Length != SaltBytes)
+            throw new ArgumentException($"Salt must be {nameof(SaltBytes)} bytes");
+
+        // Libsodiums pwhash max password length is greater than Int32.MaxValue
+        if (password.Length < PasswdMin)
+            throw new ArgumentException($"Output must be at least {nameof(PasswdMin)} bytes");
+
+        if (opsLimit is < OpsLimitMin or > OpsLimitMax)
+            throw new ArgumentException("OpsLimit count is invalid");
+
+        if (memLimit is < MemLimitMin or > MemLimitMax)
+            throw new ArgumentException("MemLimit count is invalid");
+
+        fixed (byte* k = key)
+        fixed (byte* s = salt)
+        fixed (byte* p = password)
+        {
+            var ret = crypto_pwhash(
+                k,
+                (ulong)key.Length,
+                (sbyte*)p,
+                (ulong)password.Length,
+                s,
+                opsLimit,
+                (nuint)memLimit,
+                (int)alg);
+
+            return ret == 0;
+        }
+    }
+}
+
+public enum PwHashAlgorithm
+{
+    AlgDefault = crypto_pwhash_ALG_DEFAULT,
+    AlgArgon2I13 = crypto_pwhash_ALG_ARGON2I13,
+    AlgArgon2Id13 = crypto_pwhash_ALG_ARGON2ID13,
+}
